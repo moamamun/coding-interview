ba0f98f04113570756480395624967811f710c97
Mohammed Mamun <momamun@yahoo.com>
2020-03-09 13:12:28 -0400

    steps

---
+++ common q + s/pyramidlevel.js
@@ -0,0 +1,54 @@
+// --- Directions
+// Write a function that accepts a positive number N.
+// The function should console log a pyramid shape
+// with N levels using the # character.  Make sure the
+// pyramid has spaces on both the left *and* right hand sides
+// --- Examples
+//   pyramid(1)
+//       '#'
+//   pyramid(2)
+//       ' # '
+//       '###'
+//   pyramid(3)
+//       '  #  '
+//       ' ### '
+//       '#####'
+
+// function pyramid(n) {
+//     const midpoint = Math.floor((2 * n - 1) / 2)
+
+//     for (let row = 0; row < n; row++) {
+//         let level = ''
+//         for(let colums = 0; colums < 2 * n - 1; colums++) {         
+//             if (midpoint - row <= colums && midpoint + row >= colums) {
+//                 level += '#'
+//             }
+//             else {
+//                 level +=' '
+//             }
+//         }
+//         console.log(level)
+//     }
+// }
+
+//recursion
+function pyramid(n, row = 0, level = '') {
+    if(n === row) return
+
+    if(level.length === 2 * n - 1) {
+        console.log(level)
+        return pyramid(n, row + 1)
+    }
+
+    const midpoint = Math.floor((2 * n - 1) / 2)
+    let add    
+    if (midpoint - row <= level.length && midpoint + row >= level.length) {
+        add = '#'
+    }
+    else {
+        add = ' '
+    }
+    return pyramid(n, row, level + add)
+}   
+
+console.log(pyramid(2))
